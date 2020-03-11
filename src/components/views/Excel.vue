e62bb744b0c93a17782d21c1455a2df8f6ba5e37
naufalap <32903336+naufalap@users.noreply.github.com>
2020-03-11 22:59:53 +0700

    Add files via upload

--- src/components/views/Excel.vue
+++ src/components/views/Excel.vue
@@ -1,35 +1,85 @@
 <template>
   <div>
     <div id="app" ref="spreadsheet"></div>
-    <div><input type="button" value="Add new row" @click="() => spreadsheet.insertRow()" /></div>
+    <div>
+        <input type="button" value="Add New Row" @click="() => spreadsheet.insertRow()" />
+        <input type="button" value="Delete Selected Row" @click="() => spreadsheet.deleteRow()" />
+    </div>
   </div>
 </template>
 
 <script>
 import jexcel from 'jexcel'
 import 'jexcel/dist/jexcel.css'
-var data = [
-  ['Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700'],
-  ['Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777']
-]
-var options = {
-  data: data,
-  allowToolbar: true,
-  columns: [
-    { type: 'text', title: 'Car', width: '120px' },
-    { type: 'dropdown', title: 'Make', width: '250px', source: [ 'Alfa Romeo', 'Audi', 'Bmw' ] },
-    { type: 'calendar', title: 'Available', width: '250px' },
-    { type: 'image', title: 'Photo', width: '120px' },
-    { type: 'checkbox', title: 'Stock', width: '80px' },
-    { type: 'numeric', title: 'Price', width: '100px', mask: '$ #.##,00', decimal: ',' },
-    { type: 'color', width: '100px', render: 'square' }
-  ]
-}
+import axios from 'axios'
+
 export default {
   name: 'App',
-  mounted: function () {
-    let spreadsheet = jexcel(this.$el, options)
-    Object.assign(this, { spreadsheet })
+  data() {
+    return {
+      mahasiswa: []
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load() {
+      axios.get('http://localhost:8002/api/mahasiswa/').then(response => {
+        var options = {
+          url: 'http://localhost:8002/api/mahasiswa/',
+          data: response.data,
+          oninsertrow: this.create,
+          onchange: this.update,
+          ondeleterow: this.delete,
+          allowToolbar: true,
+          columns: [
+            { type: 'hidden', title: 'id', width: '120px' },
+            { type: 'text', title: 'NRP', width: '120px' },
+            { type: 'text', title: 'Nama', width: '240px' },
+            { type: 'dropdown', title: 'Angkatan', width: '80px', source: [ '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019' ] },
+            { type: 'dropdown', title: 'jk', width: '80px', source: [ 'laki-laki', 'perempuan' ] },
+            { type: 'calendar', title: 'Tanggal Lahir', width: '100px', options: { format: 'DD/MM/YYYY' } },
+            { type: 'image', title: 'Photo', width: '120px' },
+            { type: 'checkbox', title: 'Aktif', width: '80px' }
+          ]
+        }
+        let spreadsheet = jexcel(this.$el, options)
+        Object.assign(this, { spreadsheet })
+        // handle success
+        console.log(response)
+      })
+    },
+    create() {
+      axios.post('http://localhost:8002/api/mahasiswa/', {nrp: '', nama: '', angkatan: '', jk: '', tgl: '', foto: '', aktif: ''}).then(res => {
+        console.log(res.data)
+      })
+    },
+    update(instance, cell, columns, row, value) {
+      axios.get('http://localhost:8002/api/mahasiswa/').then(res => {
+        var index = Object.values(res.data[row])
+        index[columns] = value
+        axios.put('http://localhost:8002/api/mahasiswa/' + index[0], {
+          id: index[0],
+          nrp: index[1],
+          nama: index[2],
+          angkatan: index[3],
+          jk: index[4],
+          tgl: index[5],
+          foto: index[6],
+          aktif: index[8]
+        }).then(res => {
+          console.log(res.data)
+        })
+      })
+    },
+    delete(instance, row) {
+      axios.get('http://localhost:8002/api/mahasiswa/').then(res => {
+        var index = Object.values(res.data[row])
+        console.log(row)
+        axios.delete('http://localhost:8002/api/mahasiswa/' + index[0])
+      })
+    }
   }
 }
 </script>
