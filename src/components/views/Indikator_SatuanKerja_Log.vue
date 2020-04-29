3c40e8e6e42f12a415c2119268c98781162bd777
naufalap <32903336+naufalap@users.noreply.github.com>
2020-04-29 20:04:24 +0700

    update

---
+++ src/components/views/Indikator_SatuanKerja_Log.vue
@@ -0,0 +1,121 @@
+<template>
+  <section class="content">
+    <div class="row center-block">
+      <h1 class="text-center">Indikator Satuan Kerja</h1>
+      <input type="button" value="Add New Row" @click="() => spreadsheet.insertRow()" />
+      <input type="button" value="Delete Selected Row" @click="() => spreadsheet.deleteRow()" />
+      <div id="spreadsheet"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import jexcel from 'jexcel'
+import 'jexcel/dist/jexcel.css'
+import axios from 'axios'
+var update = function (obj, cel, y, x, val) {
+  x = parseInt(x)
+  y = parseInt(y)
+  axios({
+    method: 'get',
+    url: 'http://localhost:8002/api/indikator_satuan_log/'
+  }).then(response => {
+    var data = Object.values(response.data[x])
+    console.log(response.data)
+    data[y] = val
+    var tmpData = data
+    axios.put('http://localhost:8002/api/indikator_satuan_log/' + tmpData[0], {
+      id_satker: tmpData[0],
+      id_master: tmpData[1],
+      id_periode: tmpData[2],
+      capaian: tmpData[3],
+      create_date: tmpData[4]
+    }).then(response => {
+      // console.log(response.data)
+    })
+  })
+}
+var deleterow = function(obj, x) {
+  axios({
+    method: 'get',
+    url: 'http://localhost:8002/api/indikator_satuan_log/'
+  }).then(response => {
+    var index = Object.values(response.data[x])
+    console.log(x)
+    axios.delete('http://localhost:8002/api/indikator_satuan_log/' + index[0])
+  })
+}
+var newRow = function() {
+  axios({
+    method: 'post',
+    url: 'http://localhost:8002/api/indikator_satuan_log/'
+  }).then(response => {
+    console.log(response.data)
+  })
+}
+// var options = {
+//   data: data,
+//   url: 'http://localhost:3000/siswa/',
+//   onchange: update,
+//   oninsertrow: newRow,
+//   ondeleterow: deleterow,
+//   defaultColWidth: 100,
+//   tableOverflow: true,
+//   tableWidth: '1000px',
+//   allowToolbar: false,
+//   allowInsertRow: true,
+//   allowDeleteRow: true,
+//   columns: [
+//     { type: 'hidden', title: 'ID', width: '80px' },
+//     { type: 'text', title: 'NRP', width: '200px' },
+//     { type: 'text', title: 'Nama', width: '250px' },
+//     { type: 'text', title: 'Angkatan', width: '100px' },
+//     { type: 'calendar', title: 'Tanggal Lahir', width: '120px' },
+//     { type: 'image', title: 'Photo', width: '120px' },
+//     { type: 'checkbox', title: 'Aktif', width: '80px' }
+//   ]
+// }
+export default {
+  name: 'App',
+  mounted: function () {
+    this.load()
+  },
+  // data() {
+  //   return {
+  //     siswa: [],
+  //     form: {
+  //       id: '',
+  //       nrp: '',
+  //       nama: '',
+  //       angkatan: '',
+  //       tgl_lahir: '',
+  //       foto: '',
+  //       aktif: ''
+  //     }
+  //   }
+  // },
+  methods: {
+    load() {
+      axios.get('http://localhost:8002/api/indikator_satuan_log/').then(response => {
+        console.log(response.data)
+        var options = {
+          data: response.data,
+          onchange: update,
+          oninsertrow: newRow,
+          ondeleterow: deleterow,
+          allowToolbar: true,
+          columns: [
+            { type: 'text', title: 'ID Satker', width: '150px' },
+            { type: 'text', title: 'ID Master', width: '150px' },
+            { type: 'text', title: 'ID Periode', width: '150px' },
+            { type: 'text', title: 'Capaian', width: '100px' },
+            { type: 'text', title: 'Create Update', width: '200px' }
+          ]
+        }
+        let spreadsheet = jexcel(this.$el, options)
+        Object.assign(this, { spreadsheet })
+      })
+    }
+  }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,22 @@ import NotFoundView from './components/404.vue'
 
 // Import Views - Dash
 import DashboardView from './components/views/Dashboard.vue'
-import TablesView from './components/views/Tables.vue'
 import TasksView from './components/views/Tasks.vue'
 import SettingView from './components/views/Setting.vue'
 import AccessView from './components/views/Access.vue'
 import ServerView from './components/views/Server.vue'
 import ReposView from './components/views/Repos.vue'
 import ExcelView from './components/views/Excel.vue'
+import JenisSatkerView from './components/views/JenisSatker.vue'
+import MasterIndikatorView from './components/views/MasterIndikator.vue'
+import IndikatorPeriodeView from './components/views/IndikatorPeriode.vue'
+import SatuanKerjaView from './components/views/SatuanKerja.vue'
+import PeriodeView from './components/views/Periode.vue'
+import IndikatorSatuanView from './components/views/Indikator_SatuanKerja.vue'
+import IndikatorSatuanLogView from './components/views/Indikator_SatuanKerja_Log.vue'
+import DataDasarView from './components/views/DataDasar.vue'
+import AspekView from './components/views/Aspek.vue'
+import CapaianUnitView from './components/views/Capaian_Unit.vue'
 
 // Routes
 const routes = [
@@ -29,10 +38,60 @@ const routes = [
         name: 'Dashboard',
         meta: {description: 'Overview of environment'}
       }, {
-        path: 'tables',
-        component: TablesView,
-        name: 'Tables',
-        meta: {description: 'Simple and advance table in CoPilot'}
+        path: 'excel',
+        component: ExcelView,
+        name: 'Excel',
+        meta: {description: 'Excel Satker in CoPilot'}
+      }, {
+        path: 'jenissatker',
+        component: JenisSatkerView,
+        name: 'JenisSatker',
+        meta: {description: 'Jenis Satker in CoPilot'}
+      }, {
+        path: 'masterindikator',
+        component: MasterIndikatorView,
+        name: 'MasterIndikator',
+        meta: {description: 'Master Indikator in CoPilot'}
+      }, {
+        path: 'capaian_unit',
+        component: CapaianUnitView,
+        name: 'Capaian_Unit',
+        meta: {description: 'Capaian Unit in CoPilot'}
+      }, {
+        path: 'periode',
+        component: PeriodeView,
+        name: 'Periode',
+        meta: {description: 'Periode Unit in CoPilot'}
+      }, {
+        path: 'indikatorP',
+        component: IndikatorPeriodeView,
+        name: 'IndikatorPeriode',
+        meta: {description: 'Indikator Periode Unit in CoPilot'}
+      }, {
+        path: 'satuankerja',
+        component: SatuanKerjaView,
+        name: 'SatuanKerja',
+        meta: {description: 'Satuan Kerja Unit in CoPilot'}
+      }, {
+        path: 'indikator_satuan',
+        component: IndikatorSatuanView,
+        name: 'Indikator_SatuanKerja',
+        meta: {description: 'Indikator Satuan Kerja Unit in CoPilot'}
+      }, {
+        path: 'indikator_satuan_log',
+        component: IndikatorSatuanLogView,
+        name: 'Indikator_SatuanKerja_Log',
+        meta: {description: 'Indikator Satuan Kerja Log Unit in CoPilot'}
+      }, {
+        path: 'datadasar',
+        component: DataDasarView,
+        name: 'DataDasar',
+        meta: {description: 'Data Dasar Unit in CoPilot'}
+      }, {
+        path: 'aspek',
+        component: AspekView,
+        name: 'Aspek',
+        meta: {description: 'Aspek Unit in CoPilot'}
       }, {
         path: 'tasks',
         component: TasksView,
@@ -58,11 +117,6 @@ const routes = [
         component: ReposView,
         name: 'Repository',
         meta: {description: 'List of popular javascript repos'}
-      }, {
-        path: 'Excel',
-        component: ExcelView,
-        name: 'Excel',
-        meta: {description: 'List of Excel'}
       }
     ]
   }, {
